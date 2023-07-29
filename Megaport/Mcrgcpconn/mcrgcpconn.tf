7e77c4b37bb6fe3342ae9a150c572bf0b6e87ef1
Nirmal Thewarathanthri <nthewara@gmail.com>
2023-07-29 19:54:52 +0800

    added megaport provider block for gcp interconnect

--- Megaport/Mcrgcpconn/mcrgcpconn.tf
+++ Megaport/Mcrgcpconn/mcrgcpconn.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    megaport = {
+      source = "megaport/megaport"
+      version = "0.2.10"
+    }
+  }
+}
+
 resource "megaport_gcp_connection" "gcpcon" {
   vxc_name   = "GCP VXC"
   rate_limit = 50
